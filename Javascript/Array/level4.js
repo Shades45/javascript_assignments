2141b3d95957937e323b3882db5ec4c56d80f969
Shades45 <linkerizan99@gmail.com>
2023-06-25 22:29:56 +0545

    solved level4 js and 3 questions of level5 js

---
+++ Javascript/Array/level4.js
@@ -0,0 +1,20 @@
+//Q1 find if hari is in allUsers array or not/
+//expected output is : 
+//true
+const name= 'hari'
+const allUsers = ['shyam','thakur','hari']
+//solution
+allUsers.includes(name)
+
+//Q2 find number of duplicates
+//expected output is : 2
+//true
+const user= 'hari'
+const userArr = ['hari','shyam','hari','thakur']
+//solution
+userArr.map((item)=> {
+    if(item.includes(user)){
+      return sum++
+    }
+  })
+  console.log(sum)
