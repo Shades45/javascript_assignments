adb15fd8dec77e0789dfa6c967c3c0bd96ea3b4b
Shades45 <linkerizan99@gmail.com>
2023-07-01 19:02:56 +0545

    solved level1

--- Javascript/In-Built methods/level1.js
+++ Javascript/In-Built methods/level1.js
@@ -1,1 +1,7 @@
 // write a code that generates random number from 1 to 5
+const random = (min, max) =>{
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  random(1,5)
