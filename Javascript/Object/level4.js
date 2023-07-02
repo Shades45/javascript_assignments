3e0bd6a665a47911f2d68ceb846555bd4e072c16
Shades45 <linkerizan99@gmail.com>
2023-07-02 08:51:39 +0545

    solved level4 question 1

--- Javascript/Object/level4.js
+++ Javascript/Object/level4.js
@@ -5,3 +5,21 @@ const obj = {
 }
 
 //calculate the percentage he scored in the exams out of total 150 score
+//solution
+const totalPercentage = (item) =>{
+  return (item.computer+item.maths+item.science)/150 * 100
+}
+totalPercentage(obj)
+
+const userDetails={
+    name: "kaylin",
+    maths: 23,
+    science: 35,
+    gk:30
+}
+
+// {
+//     userName: 'kaylin',
+//     subjectCodes: ['M','S', 'G'],
+//     subjectScores: [23,35,30]
+// }
